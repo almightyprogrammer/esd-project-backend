87223561465b7de256dbce349cfee0bb59c418ae
sophie0714 <jpar875@aucklanduni.ac.nz>
2025-05-09 14:15:40 +1200

    Created the profile navigation bar

--- frontend/src/views/AddNewItemPage.vue
+++ frontend/src/views/AddNewItemPage.vue
@@ -1,6 +1,9 @@
+AddNewItemPage
 <template>
     <PageHeader/>
+    <ProfileNavigationBar/>
 </template>
 <script setup>
     import PageHeader from '../components/common/PageHeader.vue';
+    import ProfileNavigationBar from '../components/profile_common/ProfileNavigationBar.vue';
 </script>
